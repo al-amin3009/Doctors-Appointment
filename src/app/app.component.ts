536f9d955337194a5f15b1ab7dcd725032e93143
Md. Abdullah Al-Amin <70529976+al-amin3009@users.noreply.github.com>
2021-09-13 20:05:54 +0600

    updated title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'doctors-appoinment';
+  title = 'doctors-appointment';
 
   
 }
